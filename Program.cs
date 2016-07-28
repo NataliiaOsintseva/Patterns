764e603599e30ed1a505386bdc28e69063a9356e
NataliiaOsintseva <nataly.osya@gmail.com>
2016-07-28 19:56:49 +0300

    Some more updates

--- Program.cs
+++ Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Factory.Factory;
+using Factory.Phone;
+using System.Reflection;
 
 namespace Factory
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+            IPhoneFactory phoneFactory = LoadFactory();
+            IPhoneBase phone = phoneFactory.CreatePhone();
+
+            phone.Ring();
+            phone.TurnOn();
+
+        }
+
+        static IPhoneFactory LoadFactory()
+        {
+            return new AndroidFactory();
+
         }
     }
 }
